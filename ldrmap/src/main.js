ce246e8407a341a27fe489589e1536efd9215ad9
Liu Tao <tao.liu@epmap.org>
2019-01-19 17:10:18 +0800

    引入weui

--- ldrmap/src/main.js
+++ ldrmap/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import MpvueRouterPatch from 'mpvue-router-patch'
 import App from '@/App'
 import store from '@/store'
+import 'mpvue-weui/src/style/weui.css';
 
 Vue.use(MpvueRouterPatch)
 Vue.config.productionTip = false
--- ldrmap/src/pages/index.vue
+++ ldrmap/src/pages/index.vue
@@ -1,55 +1,151 @@
 <template>
-    <div class="echarts-wrap">
-        <mpvue-echarts :echarts="echarts" :onInit="onInit" lazyLoad="true" canvasId="demo-canvas" />
+  <div class="container">
+    <button @click="initChart">初始化</button>
+    <div class="wrap">
+      <mpvue-echarts lazyLoad :echarts="echarts" :onInit="handleInit" ref="echarts" />
     </div>
+  </div>
 </template>
 
 <script>
-import echarts from 'echarts'
+import * as echarts from 'echarts/dist/echarts.simple.min'
 import mpvueEcharts from 'mpvue-echarts'
 import {mapState} from 'vuex'
 
-let chart = null;
-
-function initChart(canvas, width, height) {
-    chart = echarts.init(canvas, null, {
+let chart = null
+export default {
+  data () {
+    return {
+      option: null,
+      echarts
+    }
+  },
+  components: {
+    mpvueEcharts
+  },
+  methods: {
+    initChart () {
+      this.option = {
+        color: ['#37a2da', '#32c5e9', '#67e0e3'],
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: { // 坐标轴指示器，坐标轴触发有效
+            type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+          }
+        },
+        legend: {
+          data: ['热度', '正面', '负面']
+        },
+        grid: {
+          left: 20,
+          right: 20,
+          bottom: 15,
+          top: 40,
+          containLabel: true
+        },
+        xAxis: [
+          {
+            type: 'value',
+            axisLine: {
+              lineStyle: {
+                color: '#999'
+              }
+            },
+            axisLabel: {
+              color: '#666'
+            }
+          }
+        ],
+        yAxis: [
+          {
+            type: 'category',
+            axisTick: { show: false },
+            data: ['汽车之家', '今日头条', '百度贴吧', '一点资讯', '微信', '微博', '知乎'],
+            axisLine: {
+              lineStyle: {
+                color: '#999'
+              }
+            },
+            axisLabel: {
+              color: '#666'
+            }
+          }
+        ],
+        series: [
+          {
+            name: '热度',
+            type: 'bar',
+            label: {
+              normal: {
+                show: true,
+                position: 'inside'
+              }
+            },
+            data: [300, 270, 340, 344, 300, 320, 310],
+            itemStyle: {
+              emphasis: {
+                color: '#37a2da'
+              }
+            }
+          },
+          {
+            name: '正面',
+            type: 'bar',
+            stack: '总量',
+            label: {
+              normal: {
+                show: true
+              }
+            },
+            data: [120, 102, 141, 174, 190, 250, 220],
+            itemStyle: {
+              emphasis: {
+                color: '#32c5e9'
+              }
+            }
+          },
+          {
+            name: '负面',
+            type: 'bar',
+            stack: '总量',
+            label: {
+              normal: {
+                show: true,
+                position: 'left'
+              }
+            },
+            data: [-20, -32, -21, -34, -90, -130, -110],
+            itemStyle: {
+              emphasis: {
+                color: '#67e0e3'
+              }
+            }
+          }
+        ]
+      }
+      this.$refs.echarts.init()
+    },
+    handleInit (canvas, width, height) {
+      chart = echarts.init(canvas, null, {
         width: width,
         height: height
-    });
-    canvas.setChart(chart);
-
-    var option = {}; // ECharts 配置项
-
-    chart.setOption(option);
-
-    return chart; // 返回 chart 后可以自动绑定触摸操作
-}
-
-export default {
-    components: {
-        mpvueEcharts
-    },
-    data() {
-        return {
-            echarts,
-            onInit: initChart
-        }
-    },
-    computed: {
-        ...mapState([
-            'windowsWidth',
-            'windowsHeight'
-        ])
-    },
-    onLoad() {
-        console.log(this.windowsWidth, this.windowsHeight)
+      })
+      canvas.setChart(chart)
+      chart.setOption(this.option)
+      return chart
     }
+  },
+  onShareAppMessage () {}
 }
 </script>
 
 <style scoped>
-.echarts-wrap {
-    width: 100%;
-    height: 100vh;
+.container{
+  width: 100%;
+  height: 100vh;
+}
+.wrap {
+  width: 100%;
+  height: 100vh;
 }
 </style>
